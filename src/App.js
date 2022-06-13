7261cb8372bdae34b9994d128c3da5df437d2dbc
Mark Creasy <mcreasy50@gmail.com>
2022-06-13 14:44:17 -0400

    contact form submit front-end

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from './components/About'
 import Nav from './components/Nav';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact'
 import { useState } from 'react';
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
       ></Nav>
       <main>
         <div>
+          <ContactForm></ContactForm>
           <Gallery currentCategory={currentCategory}></Gallery>
           <About></About>
         </div>
