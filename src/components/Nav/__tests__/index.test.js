f7a77e195ca99101b6fd0be83376c8d31b7f15ac
Mark Creasy <mcreasy50@gmail.com>
2022-06-13 10:04:36 -0400

    nav tests

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,34 @@
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
+
+afterEach(cleanup);
+
+describe('Nav component', () => {
+  //baseline test
+  it('renders', () => {
+    render(<Nav />);
+  })
+  //snapshot test
+  it('Matches snapshot', () => {
+    const {asFragment} = render(<Nav />)
+    expect(asFragment()).toMatchSnapshot();
+  })
+})
+
+describe('Emoji is visible', () => {
+  it('inserts emoji into h2', () => {
+    const {getByLabelText} = render(<Nav />);
+      expect(getByLabelText('camera')).toHaveTextContent('📸');
+  })
+})
+
+describe('Links are visible', () => {
+
+  it('Inserts text into links', () => {
+    const {getByTestId} = render(<Nav />)
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
+    expect(getByTestId('contact')).toHaveTextContent('Contact');
+  })
+})
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -21,26 +21,26 @@ function Nav() {
   return (
     <header>
       <h2>
-        <a href="/">
+        <a data-testid="link" href="/">
           <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about">
+            <a data-testid="about" href="#about">
               About me
             </a>
           </li>
           <li>
-            <span>Contact</span>
+            <span data-testid="contact">Contact</span>
           </li>
           {categories.map((category) => (
             <li
               className="mx-1"
               key={category.name}
             >
-              <span onClick={() => categorySelected(category.name)} >
+              <span data-testid={category.name} onClick={() => categorySelected(category.name)} >
                 {category.name}
               </span>
             </li>
